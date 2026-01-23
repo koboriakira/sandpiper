03365b0d07b84bf3797d43824df68ee0dd86f516
Akira Kobori <private.beats@gmail.com>
2026-01-24 02:44:50 +0900

    feat: Add additional statuses to ToDoStatusEnum (#79)
    Co-authored-by: a-kobori <89172056+a-kobori@users.noreply.github.com>

--- src/sandpiper/shared/valueobject/todo_status_enum.py
+++ src/sandpiper/shared/valueobject/todo_status_enum.py
@@ -3,5 +3,8 @@ from enum import Enum
 
 class ToDoStatusEnum(Enum):
     TODO = "ToDo"
+    ICEBOX = "Icebox"
     IN_PROGRESS = "InProgress"
+    SUSPEND = "Suspend"
     DONE = "Done"
+    TRASH = "Trash"
